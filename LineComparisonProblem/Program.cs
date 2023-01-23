41e0588a1e61c545782e0ea847ae79b5444116e4
Dipakkamble <kambledipak700@gmail.com>
2023-01-23 13:50:51 +0530

    [Dipak] Modified calculate length of line

--- LineComparisonProblem/Program.cs
+++ LineComparisonProblem/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,17 @@ namespace LineComparisonProblem
     {
         static void Main(string[] args)
         {
+            
+            double lengthOfLine = 0;
             Console.WriteLine("Welcome to line comparison problem");
+            Console.WriteLine("Enter the value of x1 and y1");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of x2 and y2");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            lengthOfLine = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine("Length of line:" +lengthOfLine);
             Console.ReadLine();
         }
     }
